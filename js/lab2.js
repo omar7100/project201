4c320035f4e705517540847a0834366f20f215e9
omar7100 <omar.s.alshoul@gmail.com>
2019-10-07 17:48:09 +0300

    pppppppppp

---
+++ js/lab2.js
@@ -0,0 +1,54 @@
+'use strict';
+var name = prompt('what is your name?');
+alert('Hello' + name);
+
+var loc = prompt('do you knew where I\'m from ?');
+loc = loc.toLowerCase();
+// console.log("goood");
+if (loc == 'yes') {
+    alert('that\'s great');
+} else if (loc == 'y') {
+    alert('that\'s great');
+} else {
+    alert('no problem');
+}
+var color = prompt('do you knew what\'s my favorite color?');
+color = color.toLowerCase();
+// console.log("great")
+if (color == 'yes') {
+    alert('Amazing !');
+} else if (color == 'y') {
+    alert('Amazing !');
+} else {
+    alert('Ooh ! you must knew !');
+}
+var car = prompt('what\'s the kind of my car?');
+car = car.toLowerCase();
+// concole.log(very nice)
+if (car == 'yes') {
+    alert('fantastic');
+} else if (car == 'y') {
+    alert('fantastic');
+} else {
+    alert('it\'s Mercedes');
+}
+var education = prompt('do you knew what is my studies ?');
+education = education.toLowerCase();
+// concole.log(keep going !)
+   if (education == 'yes') {
+    alert('college ?');
+} else if (education == 'y') {
+    alert('college !');
+} else {
+    alert('Iam Industrial Engineering');
+}
+var job = prompt('where was my last job ?');
+job = job.toLowerCase();
+// concole.log(Excillant)
+if (hoppy == 'yes') {
+    alert('great you got it ! ');
+} else if (job == 'y') {
+    alert('great you got it !');
+} else {
+    alert('I worked in Ministry Of Municipals Affairs ');
+}
